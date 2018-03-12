c3cb8f6e1239ca9d0a8b87d9cd790f81fccc9ee1
nt <gim.kobayashi@gmail.com>
2018-03-13 04:17:36 +0900

    a

--- rust/sample.rs
+++ rust/sample.rs
@@ -14,11 +14,13 @@ use std::cmp::{Eq, Ord, PartialOrd};
 use std::collections::HashSet;
 use std::marker::Copy;
 use std::os::raw::{c_char};
+use std::ffi::CStr;
 
 #[link(name = "rocks")]
 extern {
   fn helloDB(name:*const c_char) -> ();
   fn putDB(dbName:*const c_char, key:*const c_char, value:*const c_char) -> i32;
+  fn getDB(dbName:*const c_char, key:*const c_char, value:*mut c_char) -> i32;
   
 }
 
@@ -34,7 +36,7 @@ impl Rocks {
   }
 }
 impl Rocks {
-  pub fn put(self, key:&str, value:&str) -> i32 {
+  pub fn put(&self, key:&str, value:&str) -> i32 {
     let dbName = format!("{}\0", &*(self.dbName));
     let key = format!("{}\0",  key);
     let value = format!("{}\0", value);
@@ -45,10 +47,29 @@ impl Rocks {
     sub
   }
 }
+type arr_type = [c_char; 2000];
+impl Rocks {
+  pub fn get(&self, key:&str) -> (i32,String) {
+    let dbName = format!("{}\0", &*(self.dbName));
+    let key = format!("{}\0",  key);
+    println!("{}", self.dbName);
+    println!("{}", key);
+    let mut value : arr_type = [0; 2000]; 
+    let sub = unsafe { getDB( (&*dbName).as_ptr() as *const c_char, key.as_ptr() as *const c_char, value.as_ptr() as *mut c_char) };
+    let c_str: &CStr = unsafe { CStr::from_ptr(value.as_ptr()) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    let str_buf: String = str_slice.to_owned();
+    println!("{}",sub);
 
+    println!("{}",str_buf);
+    (sub,str_buf)
+  }
+}
 
 fn main() {
   println!("hello world");
   let rocks = Rocks::new( "rocks.rdb" );
   rocks.put("some-key", "some-value");
+  let (status, value) = rocks.get("some-key");
+  println!("p {}", value);
 }
