0c4044384e23c5f27d93c721515ae03eae23cf95
nt <gim.kobayashi@gmail.com>
2018-03-13 15:00:29 +0900

    諸々フィックス

--- python/drow.cpp
+++ python/drow.cpp
@@ -1,29 +1,44 @@
 #include <boost/python.hpp>
 #include <string>
+#include <cstdio>
+#include <string>
+#include <iostream>
 
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
+using namespace rocksdb;
 namespace py = boost::python;
 
 class RDB{
+  private:
+  DB* db;
   public:
   std::string dbName;
-  RDB(std::string dbName): dbName(dbName){};
+  RDB(std::string dbName): dbName(dbName){
+    Options options;
+    options.IncreaseParallelism();
+    options.create_if_missing = true;
+    Status s = DB::Open(options, dbName, &(this->db)); 
+  };
   RDB(py::list ch);
+  void put(std::string key, std::string value);
+  std::string get(std::string key);
+  void del(std::string key);
   py::list get_chars();
 };
 
-RDB::RDB(py::list l){
-  std::string w {""};
-  std::string gap {" "};
-  std::string token;
-  for (int i = 0; i < len(l) ; i++){
-     token = py::extract<std::string>(l[i]);
-     if (i == len(l) -1){
-       w += token;
-       break;
-      }
-      w += token + gap;
-  }
-  this -> dbName = w;
+void RDB::put(std::string key, std::string value) {
+  this->db->Put(WriteOptions(), key, value);
+}
+std::string RDB::get(std::string key) {
+  std::string value;
+  auto s = this->db->Get(ReadOptions(), key, &value);
+  return std::string(value);
+}
+void RDB::del(std::string key) {
+  auto s = this->db->Delete(WriteOptions(), key);
 }
 
 py::list RDB::get_chars(){
@@ -37,7 +52,10 @@ py::list RDB::get_chars(){
 
 BOOST_PYTHON_MODULE(drow){
   py::class_<RDB>("RDB", py::init<std::string>())
-    .def(py::init<py::list>())
+    //.def(py::init<py::list>())
     .def("get_chars", &RDB::get_chars)
+    .def("put", &RDB::put)
+    .def("get", &RDB::get)
+    .def("del", &RDB::del)
     .def_readwrite("dbName", &RDB::dbName);
 }
--- python/use_drow.py
+++ python/use_drow.py
@@ -1,18 +1,12 @@
 from drow import RDB
 
 # create drow instance
-d = RDB('dog')
+db = RDB('boost.rdb')
 
 # access the word and print it
-print( d.dbName )
+print( db.dbName )
 
-# print the chars
-for char in d.get_chars():
-    print(char)
+db.put('key1', 'val1')
 
-# check out if it works for a list
-# it will join the words and save them in word variable
-d = RDB(['cat', 'dog', 'canary'])
-
-# print me joined
-print( d.dbName )
+val = db.get('key1')
+print(val)
